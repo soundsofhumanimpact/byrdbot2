aa604c6a309ad55d1c1e7833d811dbc35b327491
milesccoleman <milesccoleman@gmail.com>
2022-12-29 15:44:37 -0500

    added activity timeout

--- src/components/SoundsOfImpact.vue
+++ src/components/SoundsOfImpact.vue
@@ -404,6 +404,26 @@ export default {
       bs10: true 
     }
   },
+  
+created: function () {
+var time = new Date().getTime();
+window.addEventListener("keypress", function(event) {
+
+if (event.key !== "y") {
+    time = new Date().getTime();
+    }
+});
+
+function refresh() {
+    if(new Date().getTime() - time >= 110000) 
+        window.location.reload(true);
+    else 
+        setTimeout(refresh, 10000);
+}
+
+setTimeout(refresh, 10000);
+},
+
 mounted: function () {
 // Get the input field
 
